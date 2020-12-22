bb5ce71d2579b51fe43aa24039d5b80d95d9649d
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2020-12-22 12:28:40 -0300

    Aula 03 => Componente. Concluída.

---
+++ loiane-groner/angular/primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuPrimeiroComponent,
+    MeuPrimeiro2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
