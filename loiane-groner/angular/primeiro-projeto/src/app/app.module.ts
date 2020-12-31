8ea050f2f240b7a77690216e0be1057c37d2f66e
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2020-12-31 15:48:36 -0300

    Aula 05 => Módulos. Concluída!

--- loiane-groner/angular/primeiro-projeto/src/app/app.module.ts
+++ loiane-groner/angular/primeiro-projeto/src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { CursosModule } from './cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
 
 @NgModule({
@@ -14,7 +15,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
