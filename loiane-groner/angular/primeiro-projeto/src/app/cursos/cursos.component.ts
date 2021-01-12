fed0639da611ec6e2e7fea5e38daeab7ef20df81
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-01-12 15:19:21 -0300

    Aula 06 => Introdução aos templates. Concluída!

--- loiane-groner/angular/primeiro-projeto/src/app/cursos/cursos.component.ts
+++ loiane-groner/angular/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string
+  cursos: string[] = ['Java', 'Ext Js', 'Angular']
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training'
+    for (let i = 0; i < this.cursos.length; i++) {
+      let curso = this.cursos[i]
+    }
+  }
 
   ngOnInit(): void {
   }
