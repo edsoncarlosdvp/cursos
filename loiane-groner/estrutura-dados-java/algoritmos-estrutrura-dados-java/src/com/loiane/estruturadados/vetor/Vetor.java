0291ec091d7d318491a05bfd17246a2ad9fb5fa9
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-01-20 16:23:12 -0300

    Aulas 04 e 05 concluídas! Vetor => tamanho, imprimir e obter posição

--- loiane-groner/estrutura-dados-java/algoritmos-estrutrura-dados-java/src/com/loiane/estruturadados/vetor/Vetor.java
+++ loiane-groner/estrutura-dados-java/algoritmos-estrutrura-dados-java/src/com/loiane/estruturadados/vetor/Vetor.java
@@ -1,5 +1,7 @@
 package com.loiane.estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
 	
 	private String[] elementos;
@@ -18,21 +20,52 @@ public class Vetor {
 		}
 	}*/
 	
-	public void adiciona(String elementos) throws Exception{
+	/*public void adiciona(String elementos) throws Exception{
 		if(this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elementos;
 			this.tamanho++;
 		}else {
 			throw new Exception("O vetor já está cheio! Não é possível adicionar elementos nele!");
 		}
-	}
+	}*/
 	
-	/*public boolean adiciona(String elementos){
+	public boolean adiciona(String elementos){
 		if(this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elementos;
 			this.tamanho++;
 			return true;
 		}
 		return false;
-	}*/
+	}
+	
+	public String busca(int posicao) {
+		if(!(posicao >= 0 && posicao < this.tamanho)) {
+			throw new IllegalArgumentException("Posicao inválida!");
+		}
+		return this.elementos[posicao];
+	}
+	
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		
+		for(int i = 0; i < this.tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		
+		if(this.tamanho>0) {
+			s.append(this.elementos[this.tamanho-1]);
+			
+		}
+		
+		s.append("]");
+		return s.toString();
+	}
 }
