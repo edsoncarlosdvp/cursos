fa1e56a8ae69ec8c334912f5861af8f31240af31
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-02-16 01:23:50 -0300

    Aula 11 => Event Binding. Concluída!

--- loiane-groner/angular/primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ loiane-groner/angular/primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -8,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://www.github.com/edsoncarlosdvp';
-  cursoAngular = true;
+  cursoAngular: boolean = true;
   urlImage = 'https://image.freepik.com/fotos-gratis/belas-paisagens-do-lago-esmeralda-no-parque-nacional-yoho-columbia-britanica-canada_181624-6877.jpg';
+  valorAtual: string = '';
+  valorSalvo = '';
+  isMouseOver: boolean = false;
 
   getValor() {
     return 1;
@@ -17,6 +20,22 @@ export class DataBindingComponent implements OnInit {
 
   getCurtiCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
