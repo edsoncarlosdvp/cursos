4affcaf33e7fab36cd3ea3f468fa37b64c71b98c
Edson Carlos <edsoncarlos@equipenovainfo.com.br>
2021-03-06 19:12:28 -0300

    Aula 80 => Função Declarativa

---
+++ udemy/websites2020moderno-js6/funcao/tiposDeclaracao.js
@@ -0,0 +1,18 @@
+console.log(soma(3, 4));
+
+// FUNCTION DECLARATION
+function soma(x, y) {
+    return x + y;
+}
+
+// FUNCTION EXPRESS
+const sub = function (x, y) {
+    return x - y;
+}
+console.log(soma(3, 4));
+
+// NAMED FUNCTION EXPRESS
+const mult = function mult(x, y) {
+    return x * y;
+}
+console.log(soma(3, 4));
