505dcd6b03fbec2bb05aacff98da05f946891cd7
Edson Carlos <edsoncarlos@equipenovainfo.com.br>
2021-03-07 18:27:48 -0300

    Aula 91 => Revisão sobre Orientação a Objetos

---
+++ udemy/websites2020moderno-js6/objeto/objetoRevisao.js
@@ -0,0 +1,44 @@
+// Objeto é uma coleção dinâmica de pares chave/valor
+
+const produto = new Object
+
+produto.nome = 'Cadeira'
+produto['Marca do produto'] = 'Genérica'
+produto.preco = 220
+
+console.log(produto)
+delete produto.preco
+delete produto['Marca do produto']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 56,
+        endereco: {
+            logadouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: 'Junior',
+        idade: 19
+    }, {
+        nome: 'Ana',
+        idade: 42
+    }],
+    calcularValorSeguro: function () {
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logadouro'] = 'Av Gigante'
+
+//delete carro.condutores
+//delete carro.proprietario.endereco
+console.log(carro)
+//console.log(carro.condutores)
+//console.log(carro.condutores.lenth)
