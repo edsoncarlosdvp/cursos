5b241c1a2d0c0d89331199e3d5ff96cce058683c
edsoncarlosdvp <edsoncarlos@ymail.com>
2021-12-29 22:15:41 -0300

    Aula 145 => Concat

---
+++ udemy/websites2020moderno-js6/array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valesca', 'Cibalena']
+const filhos = ['Uesclei', 'Uoxiton']
+
+const todos = filhas.concat(filhos, 'Milene')
+console.log(todos)
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
